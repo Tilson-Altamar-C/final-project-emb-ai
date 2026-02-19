603dc533a74b8387bd68bdafd2a7f3bae06d8a6e
Tilson-Altamar-C <altamar17@gmail.com>
2026-02-19 00:23:53 -0500

    Add emotion detection function

--- emotion_detection.py
+++ emotion_detection.py
@@ -3,18 +3,9 @@ import requests
 """
     Usamos la función Emotion Predict de la biblioteca Watson NLP
 """
+
 def emotion_detector(text_to_analyze):
     
-    if not text_to_analyze:  # Verifica si viene una cadena vacia
-        return {
-            'anger': None,
-            'disgust': None,
-            'fear': None,
-            'joy': None,
-            'sadness': None,
-            'dominant_emotion': None
-        }
-
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en_stock'}
     input_json = {
@@ -22,33 +13,5 @@ def emotion_detector(text_to_analyze):
             'text': text_to_analyze
         }
     }
-
-    try:
-        response = requests.post(url, headers=headers, json=input_json)
-        if response.status_code == 400:
-            return {
-                'anger': None,
-                'disgust': None,
-                'fear': None,
-                'joy': None,
-                'sadness': None,
-                'dominant_emotion': None
-            }
-
-        response.raise_for_status()
-        response_data = response.json()
-
-        emotions = response_data.get('emotion_predictions', {})
-        emotion_scores = {
-            'anger': emotions.get('anger', 0),
-            'disgust': emotions.get('disgust', 0),
-            'fear': emotions.get('fear', 0),
-            'joy': emotions.get('joy', 0),
-            'sadness': emotions.get('sadness', 0)
-        }
-        emotion_scores['dominant_emotion'] = max(emotion_scores, key=emotion_scores.get)
-        return emotion_scores['dominant_emotion'] 
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error al conectarse Watson NLP API: {e}")
-        return None
+    response = requests.post(url, headers=headers, json=input_json)
+    return response.text
